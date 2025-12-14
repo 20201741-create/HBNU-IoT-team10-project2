9be36b3823434cff3bb7902a45492d0e7277bfbb
Eunsik Shin <20201741@edu.hanbat.ac.kr>
2025-12-14 10:10:06 +0900

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo Creating environment
+conda create --name flower python=3.8 
+
+echo Activating environment
+conda activate flower
+
+echo Fetching nesscessary packages
+python -m pip install -U --pre flwr[simulation]
+conda install pandas matplotlib jupyter
+pip install -U scikit-learn statsmodels plotly mlxtend charset-normalizer==2.1.1
+pip3 install torch torchvision torchaudio
+
+# 2025/12
+
+pip install -U "flwr[simulation]"
+pip install --upgrade kaleido
+
+mkdir -p flower/savedmodels
+mkdir -p flower/evaluation
+
+conda install "pandas<2.0.0"
